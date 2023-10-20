4fa4c8912f7e98cd4a8d025d42d5eef6485f6e1f
rkr <krostislav0906@gmail.com>
2023-10-20 15:26:10 +0300

    Create solution and describe entity "Book"

---
+++ domain/Store/Book.cs
@@ -0,0 +1,13 @@
+﻿namespace Store;
+
+public class Book
+{
+    public int Id { get; }
+    public string Title { get; }
+
+    public Book(string title, int id)
+    {
+        Title = title;
+        Id = id;
+    }
+}
